10083a6f912ad6779339fcdabe2f9e85d75dda8c
mtrussart <trussart.m@wehi.edu.au>
2023-02-08 16:59:04 +0100

    Add pca density function

---
+++ R/pca_density.R
@@ -0,0 +1,121 @@
+pca_density=function(
+        pcs,
+        pc.var,
+        group.name,
+        group,
+        color,
+        strokeSize,
+        pointSize,
+        strokeColor,
+        alpha
+){
+    pair.pcs <- utils::combn(ncol(pcs), 2)
+    pList <- list()
+    for(i in 1:ncol(pair.pcs)){
+        if(i == 1){
+            x <- pair.pcs[1,i]
+            y <- pair.pcs[2,i]
+            p <- ggplot(mapping = aes(
+                x = pcs[,x],
+                y = pcs[,y],
+                fill = group)) +
+                xlab(paste0('PC', x, ' (', pc.var[x], '%)')) +
+                ylab(paste0('PC', y, ' (', pc.var[y], '%)')) +
+                geom_point(
+                    aes(fill = group),
+                    pch = 21,
+                    color = strokeColor,
+                    stroke = strokeSize,
+                    size = pointSize,
+                    alpha = alpha) +
+                scale_x_continuous(breaks = scales::pretty_breaks(n = 5)) +
+                scale_y_continuous(breaks = scales::pretty_breaks(n = 5)) +
+                theme(
+                    legend.position = "right",
+                    panel.background = element_blank(),
+                    axis.line = element_line(colour = "black", size = 1.1),
+                    legend.background = element_blank(),
+                    legend.text = element_text(size = 12),
+                    legend.title = element_text(size = 14),
+                    legend.key = element_blank(),
+                    axis.text.x = element_text(size = 10),
+                    axis.text.y = element_text(size = 10),
+                    axis.title.x = element_text(size = 14),
+                    axis.title.y = element_text(size = 14),
+                    aspect.ratio=1) +
+                guides(fill = guide_legend(override.aes = list(size = 4))) +
+                scale_fill_manual(name = group.name, values = color)
+
+            le <- ggpubr::get_legend(p)
+        }else{
+            x <- pair.pcs[1,i]
+            y <- pair.pcs[2,i]
+            p <- ggplot(mapping = aes(
+                x = pcs[,x],
+                y = pcs[,y],
+                fill = group)) +
+                xlab(paste0('PC', x, ' (',pc.var[x],  '%)')) +
+                ylab(paste0('PC', y, ' (',pc.var[y], '%)')) +
+                geom_point(
+                    aes(fill = group),
+                    pch = 21,
+                    color = strokeColor,
+                    stroke = strokeSize,
+                    size = pointSize,
+                    alpha = alpha) +
+                scale_x_continuous(breaks = scales::pretty_breaks(n = 5)) +
+                scale_y_continuous(breaks = scales::pretty_breaks(n = 5)) +
+                theme(
+                    panel.background = element_blank(),
+                    axis.line = element_line(colour = "black", size = 1.1),
+                    legend.position = "none",
+                    axis.text.x = element_text(size = 10),
+                    axis.text.y = element_text(size = 10),
+                    axis.title.x = element_text(size = 14),
+                    axis.title.y = element_text(size = 14),
+                    aspect.ratio=1) +
+                scale_fill_manual(values = color, name = group.name)
+        }
+        p <- p + theme(legend.position = "none")
+        xdens <- cowplot::axis_canvas(p, axis = "x")+
+            geom_density(
+                mapping = aes(
+                    x = pcs[,x],
+                    fill = group),
+                alpha = 0.7,
+                size = 0.2
+            ) +
+            theme(legend.position = "none") +
+            scale_fill_manual(values = color)
+
+        ydens <- cowplot::axis_canvas(
+            p,
+            axis = "y",
+            coord_flip = TRUE) +
+            geom_density(
+                mapping = aes(
+                    x = pcs[,y],
+                    fill = group),
+                alpha = 0.7,
+                size = 0.2) +
+            theme(legend.position = "none") +
+            scale_fill_manual(name = group.name, values = color) +
+            coord_flip()
+
+        p1 <- insert_xaxis_grob(
+            p,
+            xdens,
+            grid::unit(.2, "null"),
+            position = "top"
+        )
+        p2 <- insert_yaxis_grob(
+            p1,
+            ydens,
+            grid::unit(.2, "null"),
+            position = "right"
+        )
+        pList[[i]] <- ggdraw(p2)
+    }
+    pList[[i+1]] <- le
+    return(pList)
+}
