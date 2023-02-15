5858c7ad3a427e7f0519cc5e9b6e054df517127b
mtrussart <trussart.m@wehi.edu.au>
2023-02-15 14:39:52 +0100

    test message

--- R/norm_assessment.R
+++ R/norm_assessment.R
@@ -25,7 +25,7 @@ norm_assessment = function(
         output_file=NULL
 ){
     ### Compute PCA
-    data_pca=RUVPRPS::compute_pca(sce,apply.log = apply.log)
+    #data_pca=RUVPRPS::compute_pca(sce,apply.log = apply.log)
 
     ## Get all the available normalizations methods
     normalizations <- names(
@@ -35,68 +35,73 @@ norm_assessment = function(
     ### Assessment on the biology ####
     # Color Biology
     colfunc <- colorRampPalette(brewer.pal(n = 11, name = 'Spectral')[-6])
+    message("Test print")
+    message(unique(biological_subtypes))
     color.subtype<- colfunc(length(unique(biological_subtypes)))
-    print("Test print")
     print(color.subtype)
+    message("Test print 2")
+    message(color.subtype)
     names(color.subtype) <- levels(biological_subtypes)
     print(color.subtype)
-    ### Compute PCA Biology
-    pp_bio <- lapply(
-        normalizations,
-        function(x){
-            pcs <- data_pca[[x]]
-            p1 <- RUVPRPS::pca_plot_squared(
-                pca = pcs,
-                variable= biological_subtypes,
-                variable.name =  'Biology',
-                color = color.subtype)
-            p1
-        })
-    names(pp_bio) <- normalizations
-    plot_BIO=do.call(
-        grid.arrange,
-        c(pp_bio[[1]],
-          pp_bio[[2]],
-          pp_bio[[3]],
-          pp_bio[[4]],
-          ncol = 4))
-
-    ### Assessment on the time effect ####
-    # Color Time (years)
-    colfunc <- colorRampPalette(brewer.pal(n = 4, name = 'Set1')[-6])
-    color.time <- colfunc(length(unique(time)))
-    print(color.time)
-    names(color.time) <- levels(time)
-    print(color.time)
-    ### Compute PCA Time
-    pp_time <- lapply(
-        normalizations,
-        function(x){
-            pcs <- data_pca[[x]]
-            p1 <- RUVPRPS::pca_plot_squared(
-                pca = pcs,
-                variable= time,
-                variable.name =  'Time',
-                color = color.time)
-            p1
-        })
-    names(pp_time) <- normalizations
-    plot_TIME=do.call(
-        grid.arrange,
-        c(pp_time[[1]],
-          pp_time[[2]],
-          pp_time[[3]],
-          pp_time[[4]],
-          ncol = 4))
-
-
-    ### Assessment on the library size ####
-
-    if (!is.null(output_file)){
-        pdf(output_file)
-        plot_BIO
-        plot_TIME
-        dev.off()
-    }
-    return(list(plot_bio=plot_BIO,plot_time=plot_TIME))
+    message("Test print 3")
+    # ### Compute PCA Biology
+    # pp_bio <- lapply(
+    #     normalizations,
+    #     function(x){
+    #         pcs <- data_pca[[x]]
+    #         p1 <- RUVPRPS::pca_plot_squared(
+    #             pca = pcs,
+    #             variable= biological_subtypes,
+    #             variable.name =  'Biology',
+    #             color = color.subtype)
+    #         p1
+    #     })
+    # names(pp_bio) <- normalizations
+    # plot_BIO=do.call(
+    #     grid.arrange,
+    #     c(pp_bio[[1]],
+    #       pp_bio[[2]],
+    #       pp_bio[[3]],
+    #       pp_bio[[4]],
+    #       ncol = 4))
+    #
+    # ### Assessment on the time effect ####
+    # # Color Time (years)
+    # colfunc <- colorRampPalette(brewer.pal(n = 4, name = 'Set1')[-6])
+    # color.time <- colfunc(length(unique(time)))
+    # print(color.time)
+    # names(color.time) <- levels(time)
+    # print(color.time)
+    # ### Compute PCA Time
+    # pp_time <- lapply(
+    #     normalizations,
+    #     function(x){
+    #         pcs <- data_pca[[x]]
+    #         p1 <- RUVPRPS::pca_plot_squared(
+    #             pca = pcs,
+    #             variable= time,
+    #             variable.name =  'Time',
+    #             color = color.time)
+    #         p1
+    #     })
+    # names(pp_time) <- normalizations
+    # plot_TIME=do.call(
+    #     grid.arrange,
+    #     c(pp_time[[1]],
+    #       pp_time[[2]],
+    #       pp_time[[3]],
+    #       pp_time[[4]],
+    #       ncol = 4))
+    #
+    #
+    # ### Assessment on the library size ####
+    #
+    # if (!is.null(output_file)){
+    #     pdf(output_file)
+    #     plot_BIO
+    #     plot_TIME
+    #     dev.off()
+    # }
+    # return(list(plot_bio=plot_BIO,plot_time=plot_TIME))
+    return(list("t1","t2"))
 }
