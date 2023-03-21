252b925df47aa59871466adc05f98d065ad68347
mtrussart <trussart.m@wehi.edu.au>
2023-03-21 13:27:41 +0100

    update combined plot function

--- R/plot_combined_silh.R
+++ R/plot_combined_silh.R
@@ -1,9 +1,9 @@
-#' is used to plot a combined plot of the silhouette computed on batch versus the one
-#' computed on biology of a SummarizedExperiment class object.
+#' is used to plot a combined plot of the silhouette computed on two caategorical variable
+#' of a SummarizedExperiment class object (i.e. biology vs batch).
 #'
 #'
-#' @param silh_bio Vector of the Silhouette coefficient computed based on biology.
-#' @param silh_batch Vector of the Silhouette coefficient based on batch.
+#' @param silh1 Vector of the Silhouette coefficient computed based on a categorical variable
+#' @param silh2 Vector of the Silhouette coefficient based on batch.
 
 #' @return Combined plot of the silhouette computed on batch versus the one
 #' computed on biology
@@ -11,25 +11,25 @@
 #' @import ggplot2
 #' @export
 
-plot_combined_silh_batch_bio<-function(
-        silh_bio,
-        silh_batch
+plot_combined_silh<-function(
+        silh1,
+        silh2,
 ){
 
 ## Merge two silh coeffs
-df=cbind(silh_bio,silh_batch)
-everything<-datasets<-SilhBio<-SilhBatch<-NULL
-colnames(df)=c("datasets","SilhBio","datasets","SilhBatch")
+df=cbind(silh1,silh2)
+everything<-datasets<-Silh1<-Silh2<-NULL
+colnames(df)=c("datasets","Silh1","datasets","Silh2")
 df=df[,-3]
 
 ## Plot
 dataSets.colors <- wes_palette(
         n = dim(df)[1],
         name = "GrandBudapest1")[c(1,2,4,3)]
-p=ggplot(df,aes(SilhBio,SilhBatch,colour=datasets)) +
+p=ggplot(df,aes(Silh2Silh2colour=datasets)) +
     geom_point(aes(color = datasets), size = 3) +
     xlab('Silhouette based on Biology') +
-    ylab ('Silhouette based on Bacth') +
+    ylab ('Silhouette based on Batch') +
     scale_color_manual(
         values=c(dataSets.colors),
         name = 'Datasets') +
