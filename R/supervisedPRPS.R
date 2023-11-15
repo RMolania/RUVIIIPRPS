a6839c78c108c0fa5e1613d5a11c0a2ace23821d
mtrussart <trussart.m@wehi.edu.au>
2023-11-15 17:58:17 +0100

    Update arguments definition, cont.coef and min.sample.for.prps

--- R/supervisedPRPS.R
+++ R/supervisedPRPS.R
@@ -1,48 +1,60 @@
 #' is used to create pseudo-replicates of pseudo samples of a SummarizedExperiment class object
-#' using RUVIII-PRPS method with a supervised approach using the 'uv.variables' and 'bio.variable' given.
+#' using RUVIII-PRPS method with a supervised approach using the 'uv.variables', 'bio.variable' and 'batch.variable' given.
 #'
 #' We will create distinct group of pseudo-replicates for each source of unwanted variation defined in the 'uv.variables' argument.
 #' For example to correct for batch effect if defined in the 'uv.variables' argument, several group of pseudo-samples
 #' will be created by averaging the samples of the same biological subtype defined in 'bio.variable' in each batch. Then those
-#' pseudo-samples will be defined as pseudo-replicates.
+#' pseudo-samples will be defined as pseudo-replicates which constitutes a PRPS set.
 #' For example to correct for library size if defined in the 'uv.variables' argument, several group of pseudo-samples
 #' will be created by averaging the top and bottom-ranked samples by library size of the same biological subtype in each batch.
-#' Then those pseudo-samples will be defined as pseudo-replicates.
+#' Then those pseudo-samples will be defined as pseudo-replicates which constitutes a PRPS set.
 #' Similarly to correct for purity if defined in the 'purity' argument, several group of pseudo-samples
 #' will be created by averaging the top and bottom-ranked samples by purity of the same biological subtype in each batch.
-#' Then those pseudo-samples will be defined as pseudo-replicates.
+#' Then those pseudo-samples will be defined as pseudo-replicates which constitutes a PRPS set.
 #'
 #'
-#' @param se.obj A summarized experiment object.
-#' @param assay.name String for the selection of the name of the assay of the SummarizedExperiment class object used to define PRPS.
-#' We recommend to use the raw data assay.
+#' @param se.obj A SummarizedExperiment object that will be used to create PRPS.
+#' @param assay.name String for the selection of the name of the assay of the SummarizedExperiment class object.
 #' @param bio.variable String of the label of a categorical variable that specifies major biological groups
 #' such as samples types from colData(se).
 #' @param uv.variables String or vector of strings of the label of continuous or categorical variable(s)
 #' such as samples types, batch or library size from colData(se) that will be used to define PRPS.
 #' @param batch.variable String of the label of a categorical variable that specifies major batch groups
 #' such as plates from colData(se).
-#' @param min.sample.prps TO BE DEFINED.
-#' @param min.sample.per.batch TO BE DEFINED.
+#' @param min.sample.for.prps Numeric. Indicates the minimum number of samples to create one pseudo-sample,
+#' by default it is set to 3.
+#' @param min.sample.per.batch Numeric. Indicates the minimum number of homogeneous biological samples within each batch
+#' to create a PRPS set for each continuous variable. The minimum should be '2*min.sample.for.prps'. By default it is set to 6.
 #' @param apply.log Logical. Indicates whether to apply a log-transformation to the data, by default it is set to TRUE.
 #' @param pseudo.count Numeric. A value as a pseudo count to be added to all measurements before log transformation,
 #' by default it is set to 1.
 #' @param assess.se.obj Logical. Indicates whether to assess the SummarizedExperiment class object.
 #' By default it is set to TRUE.
-#' @param assess.cor.variables Logical. Indicates whether to assess the assess the association between variables
-#' using Spearman correlation.
-#' @param cont.coef TO BE DEFINED.
-#' @param spearman.coef TO BE DEFINED.
-#' @param remove.na TO BE DEFINED.
+#' @param assess.cor.variables Logical. Indicates whether to assess the association between pairs of categorical variables
+#' and/or pairs of continuous variables in order to select only one of the variable of a pair of highly correlated
+#' variables.
+#' @param cat.cor.coef Vector of two numerical values. Indicates the minimum cut-off of the correlation coefficient between each pair of
+#' categorical variables. The first one is between each pair of 'uv.variables' and the second one is between each pair of 'bio.variables'.
+#' The correlation is computed by the function ContCoef from the DescTools package. If the correlation of a pair of variable is higher than
+#' the cut-off, then only the variable that has the highest number of factor will be kept and the other one will be excluded from the
+#' remaining analysis. By default they are both set to 0.85.
+#' @param cont.cor.coef Vector of two numerical values. Indicates the minimum cut-off of the Spearman correlation coefficient between each pair of
+#' continuous variables. The first one is between each pair of 'uv.variables' and the second one is between each pair of 'bio.variables'.
+#' If the correlation of a pair of variable is higher than the cut-off, then only the variable that has the highest variance will
+#' be kept and the other one will be excluded from the remaining analysis. By default they are both set to 0.85.
+#' @param remove.na String. Indicates whether to remove NA or missing values from either the 'assays', the 'sample.annotation',
+#' 'both' or 'none'. If 'assays' is selected, the genes that contains NA or missing values will be excluded. If 'sample.annotation' is selected, the
+#' samples that contains NA or missing values for any 'bio.variables' and 'uv.variables' will be excluded. By default, it is set to
+#' 'both'.
 #' @param save.se.obj Logical. Indicates whether to save the result in the metadata of the SummarizedExperiment
 #' class object 'se.obj' or to output the result, by default it is set to TRUE.
-#' @param plot.output Logical. Indicates whether to plot the PRPS map define by the 'bio.variable'
-#' colored by the categorical variables, by default it is set to TRUE.
+#' @param plot.output Logical. Indicates whether to plot the PRPS map of the PRPS defined by the 'bio.variable'
+#' and the categorical variables, by default it is set to TRUE.
 #' @param verbose Logical. Indicates whether to show or reduce the level of output or messages displayed
 #' during the execution of the functions, by default it is set to TRUE.
 
-#' @return SummarizedExperiment or a List A SummarizedExperiment object or a list containing
-#' all the prps defined.
+#' @return SummarizedExperiment or a List A SummarizedExperiment object or a list containing all the PRPS created.
+
 #' @importFrom SummarizedExperiment assay colData
 #' @importFrom dplyr count
 #' @importFrom tidyr %>%
@@ -54,14 +66,14 @@ supervisedPRPS <- function(
         bio.variable,
         uv.variables,
         batch.variable,
-        min.sample.prps = 3,
-        min.sample.per.batch = 10,
+        min.sample.for.prps = 3,
+        min.sample.per.batch = 6,
         apply.log = TRUE,
         pseudo.count = 1,
         assess.se.obj = TRUE,
         assess.cor.variables = FALSE,
-        cont.coef = c(0.7, 0.7),
-        spearman.coef = c(0.7, 0.7),
+        cat.cor.coef = c(0.85, 0.85),
+        cont.cor.coef = c(0.85, 0.85),
         remove.na = 'both',
         save.se.obj = TRUE,
         plot.output=TRUE,
@@ -81,23 +93,22 @@ supervisedPRPS <- function(
         stop('The function requires a batch variable.')
     }
 
-    ### Assess the input
-    # ######### THIS PARt NEED to BE tEStED ############
-    # if(assess.cor.variables){
-    #     se.obj <- variablesCorrelation(
-    #         se.obj = se.obj,
-    #         assay.name = assay.name,
-    #         bio.variables = bio.variable,
-    #         uv.variables = uv.variables,
-    #         cont.coef = cont.coef,
-    #         spearman.coef = spearman.coef,
-    #         assess.se.obj = FALSE,
-    #         remove.na = remove.na,
-    #         verbose = verbose)
-    #     uv.variables <- se.obj$uv.variables
-    #     bio.variable <- se.obj$bio.variable
-    #     se.obj <- se.obj$se.obj
-    # }
+    ### Assess the correlation btw variables
+    if(assess.cor.variables){
+        se.obj <- variablesCorrelation(
+            se.obj = se.obj,
+            assay.name = assay.name,
+            bio.variables = bio.variable,
+            uv.variables = uv.variables,
+            cat.cor.coef = cat.cor.coef,
+            cont.cor.coef = cont.cor.coef,
+            assess.se.obj = assess.se.obj,
+            remove.na = remove.na,
+            verbose = verbose)
+        uv.variables <- se.obj$uv.variables
+        bio.variable <- se.obj$bio.variable
+        se.obj <- se.obj$se.obj
+    }
 
     ### Define categorical and continuous variables
     uv.class <- sapply(
@@ -123,7 +134,7 @@ supervisedPRPS <- function(
                         assay.name = assay.name,
                         uv.variable = x,
                         bio.variable = bio.variable,
-                        min.sample.prps = min.sample.prps,
+                        min.sample.for.prps = min.sample.for.prps,
                         apply.log = apply.log,
                         assess.se.obj = FALSE,
                         save.se.obj = save.se.obj,
@@ -141,7 +152,7 @@ supervisedPRPS <- function(
                     assay.name = assay.name,
                     uv.variable = x,
                     bio.variable = bio.variable,
-                    min.sample.prps = min.sample.prps,
+                    min.sample.for.prps = min.sample.for.prps,
                     apply.log = apply.log,
                     assess.se.obj = FALSE,
                     save.se.obj = TRUE,
@@ -169,7 +180,7 @@ supervisedPRPS <- function(
                         uv.variable = x,
                         bio.variable = bio.variable,
                         batch.variable = batch.variable,
-                        min.sample.prps = min.sample.prps,
+                        min.sample.for.prps = min.sample.for.prps,
                         min.sample.per.batch=min.sample.per.batch,
                         apply.log = apply.log,
                         assess.se.obj = FALSE,
@@ -189,7 +200,7 @@ supervisedPRPS <- function(
                     uv.variable = x,
                     bio.variable = bio.variable,
                     batch.variable = batch.variable,
-                    min.sample.prps = min.sample.prps,
+                    min.sample.for.prps = min.sample.for.prps,
                     min.sample.per.batch=min.sample.per.batch,
                     apply.log = apply.log,
                     assess.se.obj = FALSE,
@@ -201,7 +212,6 @@ supervisedPRPS <- function(
             }
         }
     }
-
     #### Ploting output
     if (plot.output==TRUE && (length(categorical.uv) > 0)){
         ## PRPS map plotting
@@ -219,7 +229,7 @@ supervisedPRPS <- function(
                 df_count <- info %>%
                     count(catvar, biology)
                 df_count$use <- 'unselected'
-                df_count$use[df_count$n >= min.sample.prps] <- 'Selected'
+                df_count$use[df_count$n >= min.sample.for.prps] <- 'Selected'
                 p=ggplot(df_count, aes(x = catvar, y = biology)) +
                     geom_count(aes(color = use)) +
                     geom_text(aes(
