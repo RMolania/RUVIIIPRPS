8c4828eb4d8c4e8fc8c73f586e74555fd2dc99cb
RMolania <ramyar.molania@gmail.com>
2023-12-15 15:26:28 +1100

    tidy up plotPCA

--- R/plotPCA.R
+++ R/plotPCA.R
@@ -1,5 +1,4 @@
-#' is used to plot the pairwise plots of the first PCA of the gene expression (assay)
-#' of a SummarizedExperiment class object.
+#' is used to plot the pairwise plots of the first principal components of the assays of a SummarizedExperiment object.
 #'
 #'
 #' @param se.obj A SummarizedExperiment object that will be used to compute the PCA.
@@ -30,51 +29,51 @@
 #' @export
 
 
-plotPCA=function(se.obj,
-                 assay.names='All',
-                 variable,
-                 color=NULL,
-                 fast.pca = TRUE,
-                 nb.pcs = 3,
-                 ncol.plot=4,
-                 strokeSize = .2,
-                 pointSize = 1.5,
-                 strokeColor = 'gray30',
-                 alpha = .5,
-                 assess.se.obj = TRUE,
-                 verbose = TRUE
-){
+plotPCA <- function(
+        se.obj,
+        assay.names = 'All',
+        variable,
+        color = NULL,
+        fast.pca = TRUE,
+        nb.pcs = 3,
+        ncol.plot = 4,
+        strokeSize = .2,
+        pointSize = 1.5,
+        strokeColor = 'gray30',
+        alpha = .5,
+        assess.se.obj = TRUE,
+        verbose = TRUE
+) {
     printColoredMessage(message = '------------The plotPCA function starts:',
                         color = 'white',
                         verbose = verbose)
 
-    ### check the inputs
+    # check the inputs ####
     if (is.null(nb.pcs)) {
         stop('To plot the PCA, the number of PCs (nb.pcs) must be specified.')
     } else if (is.null(assay.names)) {
         stop('Please provide at least an assay name.')
     } else if (is.null(variable)) {
-        stop('Please provide a variable.')
+        stop('Please provide a categorical variable.')
     } else if (class(se.obj@colData[, variable]) %in% c('numeric', 'integer')) {
         stop(paste0('The ', variable,', is a numeric, but this should a categorical variable'))
     }
 
-    ### Assess the se.obj
-    if (assess.se.obj){
-        se.obj <- checkSeObj(se.obj = se.obj,
-                             assay.names = assay.names,
-                             variables = variable,
-                             remove.na = 'both',
-                             verbose = verbose)
+    # find assays ####
+    if (length(assay.names) == 1 && assay.names == 'All') {
+        assay.names <- as.factor(names(assays(se.obj)))
+    } else {
+        assay.names <- as.factor(unlist(assay.names))
     }
-
-    ## Assays
-    if(length(assay.names) == 1 && assay.names=='All'){
-        assay.names=as.factor(names(assays(se.obj)))
-    }else {
-        assay.names=as.factor(unlist(assay.names))
+    # assess the SummarizedExperiment object ####
+    if (assess.se.obj) {
+        se.obj <- checkSeObj(
+            se.obj = se.obj,
+            assay.names = assay.names,
+            variables = variable,
+            remove.na = 'none',
+            verbose = verbose)
     }
-
 
     if (fast.pca) {
         ppca <- lapply(
@@ -83,18 +82,19 @@ plotPCA=function(se.obj,
                 if (!'fastPCA' %in% names(se.obj@metadata[['metric']][[x]])) {
                     stop('To plot the PCA, the fast PCA must be computed first on the assay ', x, ' .')
                 }
-                pca_x <- se.obj@metadata[['metric']][[x]][['fastPCA']]
-                p1=plotPCAassay(
-                    se.obj=se.obj,
-                    pca_x=pca_x,
-                    variable=variable,
+                pca.data  <- se.obj@metadata[['metric']][[x]][['fastPCA']]
+                p1 <- plotPCAassay(
+                    se.obj = se.obj,
+                    pca_x = pca.data,
+                    variable = variable,
                     nb.pcs = nb.pcs,
                     fast.pca = TRUE,
-                    color=color,
-                    strokeSize=strokeSize,
-                    pointSize=pointSize,
-                    strokeColor=strokeColor,
-                    alpha=alpha)
+                    color = color,
+                    strokeSize = strokeSize,
+                    pointSize = pointSize,
+                    strokeColor = strokeColor,
+                    alpha = alpha
+                )
                 p1
             })
         names(ppca) <- levels(assay.names)
@@ -103,44 +103,41 @@ plotPCA=function(se.obj,
         ppca <- lapply(
             levels(assay.names),
             function(x) {
-                if (!'PCA' %in% names(se.obj@metadata[['metric']][[x]])) {
+                if (!'PCA' %in% names(se.obj@metadata[['metric']][[x]]))
                     stop('To plot the PCA, the PCA must be computed first on the assay ', x, ' .')
-                }
-                pca_x <- se.obj@metadata[['metric']][[x]][['PCA']]
-                p1=plotPCAassay(
-                    se.obj=se.obj,
-                    pca_x=pca_x,
-                    variable=variable,
+                pca.data <- se.obj@metadata[['metric']][[x]][['PCA']]
+                p1 <- plotPCAassay(
+                    se.obj = se.obj,
+                    pca_x = pca.data,
+                    variable = variable,
                     nb.pcs = nb.pcs,
                     fast.pca = FALSE,
-                    color=color,
-                    strokeSize=strokeSize,
-                    pointSize=pointSize,
-                    strokeColor=strokeColor,
-                    alpha=alpha)
+                    color = color,
+                    strokeSize = strokeSize,
+                    pointSize = pointSize,
+                    strokeColor = strokeColor,
+                    alpha = alpha
+                )
                 p1
             })
         names(ppca) <- levels(assay.names)
-
     }
-
-        ## Prepare plot
-        p=ppca[[levels(assay.names)[1]]]
-        if (length(assay.names)>1){
-            for (n in 2:length(assay.names)){
-                p=c(p,ppca[[levels(assay.names)[n]]])
-            }
+    ## Prepare plot
+    p = ppca[[levels(assay.names)[1]]]
+    if (length(assay.names) > 1) {
+        for (n in 2:length(assay.names)) {
+            p = c(p, ppca[[levels(assay.names)[n]]])
         }
-        if (fast.pca) {
-            title=paste0("FastPCA ordered as ", paste(levels(assay.names), collapse = ","))
-        } else{
-            title=paste0("PCA ordered as ", paste(levels(assay.names), collapse = ","))
-        }
-        plot=do.call(grid.arrange,
-            c(p,
-              ncol = ncol.plot,
-              top=title))
+    }
+    if (fast.pca) {
+        title = paste0("FastPCA ordered as ", paste(levels(assay.names), collapse = ","))
+    } else{
+        title = paste0("PCA ordered as ", paste(levels(assay.names), collapse = ","))
+    }
+    plot = do.call(grid.arrange,
+                   c(p,
+                     ncol = ncol.plot,
+                     top = title))
 
-        return(plot=as_ggplot(plot))
+    return(plot = as_ggplot(plot))
 }
-
