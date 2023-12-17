c62222b9746cd2edeb523a292d89b732068ea77a
RMolania <ramyar.molania@gmail.com>
2023-12-17 18:41:04 +1100

    remove apply.round and add lib

--- R/PCVariableRegression.R
+++ R/PCVariableRegression.R
@@ -16,193 +16,163 @@
 #' @param plot.output Logical. Indicates whether to plot the regression statistics, by default it is set to TRUE.
 #' @param assess.se.obj Logical. Indicates whether to assess the SummarizedExperiment class object.
 #' @param remove.na TO BE DEFINED.
-#' @param apply.round Logical. Indicates whether to round the ARI results, by default it is set to TRUE.
 #' @param verbose Logical. Indicates whether to show or reduce the level of output or messages displayed during the execution
 #' of the functions, by default it is set to TRUE.
 #'
 #' @return SummarizedExperiment A SummarizedExperiment object containing the computed regression for
 #' the continuous variable and if requested the associated plot.
-#' @importFrom stats lm
-#' @importFrom wesanderson wes_palette
+#' @importFrom stats lm var
 #' @importFrom dplyr rename mutate
 #' @importFrom tidyr pivot_longer %>%
 #' @import ggplot2
 #' @export
 
 ## deal with PCA and remove NA from variable
-PCVariableRegression<-function(
+PCVariableRegression <- function(
         se.obj,
         assay.names = 'All',
         variable,
         fast.pca = TRUE,
         nb.pcs = 10,
         save.se.obj = TRUE,
-        plot.output=TRUE,
+        plot.output = TRUE,
         assess.se.obj = TRUE,
         remove.na = 'both',
-        apply.round = TRUE,
         verbose = TRUE
-){
-
+) {
     printColoredMessage(message = '------------The PCVariableRegression function starts:',
                         color = 'white',
                         verbose = verbose)
 
-    ### check the inputs
+    # check the inputs ####
     if (is.null(assay.names)) {
-        stop('Please provide at least an assay name.')
-    } else if (is.null(variable)) {
-        stop('Please provide a variable.')
+        stop('The "assay.names" cannot be null.')
+    }
+    if(length(assay.names) == 1 & assay.names!= 'All'){
+        if(!assay.names %in% names(assays(se.obj)) )
+            stop('The assay name cannot be found in the SummarizedExperiment object.')
+    }
+    if(length(assay.names) > 1){
+        if(!assay.names %in% names(assays(se.obj)))
+            stop('The assay names cannot be found in the SummarizedExperiment object.')
+    }
+    if (is.null(variable)) {
+        stop('The variable cannot be empty.')
     } else if (!class(se.obj@colData[, variable]) %in% c('numeric', 'integer')) {
-        stop(paste0('The ', variable,', is not numeric, but this should a continuous variable'))
+        stop( paste0( 'The ', variable, 'this should a continuous variable'))
     } else if (is.null(nb.pcs)) {
         stop('To compute the regression, the number of PCs (nb.pcs) must be specified.')
     }
-
-    ### Assess the se.obj
-    if (assess.se.obj){
-        se.obj <- checkSeObj(se.obj = se.obj,
-                             assay.names = assay.names,
-                             variables = variable,
-                             remove.na = remove.na,
-                             verbose = verbose)
-    }
-
-
-    ### Check if the variable provided has a variance of 0:
-    if(var(se.obj[[variable]]) == 0){
+    if (var(se.obj[[variable]]) == 0) {
         stop(paste0('The ', variable, ' appears to have no variation.'))
     }
-    var=se.obj@colData[, variable]
 
-    ## Assays
-    if(length(assay.names) == 1 && assay.names=='All'){
-        assay.names=as.factor(names(assays(se.obj)))
-    }else {
-        assay.names=as.factor(unlist(assay.names))
+    # assays ####
+    if (length(assay.names) == 1 && assay.names == 'All') {
+        assay.names = as.factor(names(assays(se.obj)))
+    } else assay.names = as.factor(unlist(assay.names))
+
+    # assess the SummarizedExperiment ####
+    if (assess.se.obj) {
+        se.obj <- checkSeObj(
+            se.obj = se.obj,
+            assay.names = assay.names,
+            variables = variable,
+            remove.na = remove.na,
+            verbose = verbose)
     }
-
-    if (fast.pca) {
-        ### Compute the regression on all assays
-        lreg.pcs<- lapply(
-            levels(assay.names),
-            function(x){
-                if (!'fastPCA' %in% names(se.obj@metadata[['metric']][[x]])) {
-                    stop('To compute the regression,
-                         the fast PCA must be computed first on the assay ', x, ' .')
-                }
-                ### Regression on PCs and continous variable
-                printColoredMessage(message = paste0(
-                    '### Computing regression based on PCs and the ',
+    ### Compute the regression on all assays
+    printColoredMessage(
+        message = '-- Apply regression:',
+        color = 'magenta',
+        verbose = verbose)
+    all.r.squared <- lapply(
+        levels(assay.names),
+        function(x) {
+            printColoredMessage(
+                message = paste0(
+                    '-- Compute R^2 of the regression between PCs of the ',
+                    x ,
+                    ' and ',
                     variable,
-                    ' variable.'
-                ),
-                color = 'magenta',
+                    ' variable.'),
+                color = 'blue',
                 verbose = verbose)
-                pca_x <- se.obj@metadata[['metric']][[x]][['fastPCA']]$sing.val$u[colnames(se.obj),]
-                rSquared <- sapply(
-                    1:nb.pcs,
-                    function(y) {
-                        lm.ls <- summary(lm(
-                            var ~ pca_x[, 1:y])
-                        )$r.squared
-                    })
-                return(rSquared)
-            })
-        names(lreg.pcs) <- levels(assay.names)
-    } else{
-        ### Compute the regression on all assays
-        lreg.pcs<- lapply(
-            levels(assay.names),
-            function(x){
-                if (!'PCA' %in% names(se.obj@metadata[['metric']][[x]])) {
-                    stop('To compute the regression,
-                         the PCA must be computed first on the assay ', x, ' .')
-                }
-                ### Regression on PCs and continous variable
-                printColoredMessage(message = paste0(
-                    '### Computing regression based on PCs and the ',
-                    variable,
-                    ' variable.'
-                ),
-                color = 'magenta',
-                verbose = verbose)
-                pca_x <- se.obj@metadata[['metric']][[x]][['PCA']]$sing.val$u[colnames(se.obj),]
-                rSquared <- sapply(
-                    1:nb.pcs,
-                    function(y) {
-                        lm.ls <- summary(lm(
-                            var ~ pca_x[, 1:y])
-                        )$r.squared
-                    })
-                return(rSquared)
-            })
-        names(lreg.pcs) <- levels(assay.names)
-    }
+            if (fast.pca) {
+                if (!'fastPCA' %in% names(se.obj@metadata[['metric']][[x]]))
+                    stop('To compute the regression,the fast PCA must be computed first on the assay ', x, ' .')
+                pca.data <-
+                    se.obj@metadata[['metric']][[x]][['fastPCA']]$sing.val$u[colnames(se.obj),]
+            } else {
+                if (!'PCA' %in% names(se.obj@metadata[['metric']][[x]]))
+                    stop('To compute the regression, the PCA must be computed first on the assay ', x, ' .')
+                pca.data <- se.obj@metadata[['metric']][[x]][['PCA']]$sing.val$u[colnames(se.obj), ]
+            }
+            r.squared <- sapply(
+                1:nb.pcs,
+                function(y) lm.ls <- summary(lm(se.obj@colData[, variable] ~ pca.data[, 1:y]))$r.squared)
+            return(r.squared)
+        })
+    names(all.r.squared) <- levels(assay.names)
 
-    ## Round the regression statistic obtained to 2 digits
-    if(apply.round){
-        lreg.pcs[] <- lapply(lreg.pcs, round,3)
-    }
-
-
-    ### Add results to the SummarizedExperiment object
-    if(save.se.obj == TRUE){
-        printColoredMessage(message= '### Saving the regression results to the metadata of the SummarizedExperiment object.',
-                            color = 'magenta',
-                            verbose = verbose)
-
-        for (x in levels(assay.names)){
-            ## Check if metadata metric already exist
-            if(length(se.obj@metadata)==0 ) {
+    # save the results ####
+    ## add results to the SummarizedExperiment object ####
+    if (save.se.obj == TRUE) {
+        printColoredMessage(
+            message = '-- Save the regression results to the metadata of the SummarizedExperiment object.',
+            color = 'magenta',
+            verbose = verbose)
+        for (x in levels(assay.names)) {
+            ## check if metadata metric already exist
+            if (length(se.obj@metadata) == 0) {
                 se.obj@metadata[['metric']] <- list()
             }
-            ## Check if metadata metric already exist for this assay
-            if(!'metric' %in% names(se.obj@metadata) ) {
+            ## check if metadata metric already exist for this assay
+            if (!'metric' %in% names(se.obj@metadata)) {
                 se.obj@metadata[['metric']] <- list()
             }
-            ## Check if metadata metric already exist for this assay
-            if(!x %in% names(se.obj@metadata[['metric']]) ) {
+            ## check if metadata metric already exist for this assay
+            if (!x %in% names(se.obj@metadata[['metric']])) {
                 se.obj@metadata[['metric']][[x]] <- list()
             }
-            ## Check if metadata metric already exist for this assay and this metric
-            if(!'pcs.lm' %in% names(se.obj@metadata[['metric']][[x]])  ) {
+            ## check if metadata metric already exist for this assay and this metric
+            if (!'pcs.lm' %in% names(se.obj@metadata[['metric']][[x]])) {
                 se.obj@metadata[['metric']][[x]][['pcs.lm']] <- list()
             }
             ## Check if metadata metric already exist for this assay, this metric and this variable
-            #if(!variable %in% names(se.obj@metadata[['metric']][[x]][['pcs.lm']])  ) {
-                se.obj@metadata[['metric']][[x]][['pcs.lm']][[variable]] <- lreg.pcs[[x]]
-            #}
+            se.obj@metadata[['metric']][[x]][['pcs.lm']][[variable]] <- all.r.squared[[x]]
         }
-        printColoredMessage(message= paste0(
-            'The regression results are saved to metadata@metric$',
-            x,
-            '$pcs.lm$',
-            variable, '.'),
+        printColoredMessage(
+            message = 'The regression results for individal assays are saved to metadata@metric',
             color = 'blue',
-            verbose = verbose)
-
+            verbose = verbose
+            )
         ## Plot and save the plot into se.obj@metadata$plot
-        if (plot.output==TRUE) {
-            printColoredMessage(message= '### Plotting and Saving the regression results to the metadata of the SummarizedExperiment object.',
-                                color = 'magenta',
-                                verbose = verbose)
-
-            se.obj=plotMetric(se.obj,
-                              assay.names =assay.names,
-                              metric='pcs.lm',
-                              variable=variable,
-                              verbose=verbose)
+        if (plot.output == TRUE) {
+            printColoredMessage(
+                message = '-- Plot the the regression results:',
+                color = 'magenta',
+                verbose = verbose)
+            printColoredMessage(
+                message = 'A plot of the R^2 of the regression are saved to metadata@plot.',
+                color = 'blue',
+                verbose = verbose)
+            se.obj <- plotMetric(
+                se.obj,
+                assay.names = assay.names,
+                metric = 'pcs.lm',
+                variable = variable,
+                verbose = verbose)
         }
+        printColoredMessage(message = '------------The PCVariableRegression function finished.',
+                            color = 'white',
+                            verbose = verbose)
         return(se.obj = se.obj)
-
-        ## Return only the correlation result
-    } else if(save.se.obj == FALSE){
-        return(lreg.pcs=lreg.pcs)
+        ## return only the regression results ####
+    } else if (save.se.obj == FALSE) {
+        printColoredMessage(message = '------------The PCVariableRegression function finished.',
+                            color = 'white',
+                            verbose = verbose)
+        return(all.r.squared = all.r.squared)
     }
-
-    printColoredMessage(message = '------------The PCVariableRegression function finished.',
-                        color = 'white',
-                        verbose = verbose)
-
 }
