dc6d4056d05c2324a95bf4afa56467025854b695
RMolania <ramyar.molania@gmail.com>
2024-01-04 19:54:44 +1100

    change the look of the function

--- R/plotCombinedSilhouette.R
+++ R/plotCombinedSilhouette.R
@@ -25,21 +25,26 @@
 #' @export
 
 
-plotCombinedSilhouette<-function(
-                    se.obj,
-                    assay.names='All',
-                    variable1,
-                    variable2,
-                    method='euclidian',
-                    save.se.obj = TRUE,
-                    assess.se.obj = TRUE,
-                    verbose=TRUE
-){
+plotCombinedSilhouette <- function(
+        se.obj,
+        assay.names = 'All',
+        variable1,
+        variable2,
+        method = 'euclidian',
+        save.se.obj = TRUE,
+        assess.se.obj = TRUE,
+        verbose = TRUE) {
     ## variables = c(variable1,variable2)
     if (is.null(assay.names)) {
         stop('Please provide at least an assay name.')
     } else if (class(se.obj@colData[, variable1]) %in% c('numeric', 'integer')) {
-    stop(paste0('The ', variable1,', is a numeric, but this should a categorical variable'))
+        stop(
+            paste0(
+                'The ',
+                variable1,
+                ', is a numeric, but this should a categorical variable'
+            )
+        )
     }
 
 
@@ -53,86 +58,105 @@ plotCombinedSilhouette<-function(
     }
 
     ### Assess the se.obj
-    if (assess.se.obj){
-        se.obj <- checkSeObj(se.obj = se.obj,
-                             assay.names = assay.names,
-                             variables = c(variable1,variable2),
-                             remove.na = 'both',
-                             verbose = verbose)
+    if (assess.se.obj) {
+        se.obj <- checkSeObj(
+            se.obj = se.obj,
+            assay.names = assay.names,
+            variables = c(variable1, variable2),
+            remove.na = 'both',
+            verbose = verbose
+        )
     }
 
     ## Assays
-    if(length(assay.names) == 1 && assay.names=='All'){
-        assay.names=as.factor(names(assays(se.obj)))
-    }else {
-        assay.names=as.factor(unlist(assay.names))
+    if (length(assay.names) == 1 && assay.names == 'All') {
+        assay.names = as.factor(names(assays(se.obj)))
+    } else {
+        assay.names = as.factor(unlist(assay.names))
     }
 
     ## Merge two silh coeffs
     all.assays.var1 <- lapply(
         levels(assay.names),
-        function(x){
-            if (!variable1 %in% names(se.obj@metadata[['metric']][[x]][[paste0('sil.',method)]])) {
-                stop(paste0('The Silhouette based on ',method,' has not been computed yet for the ',variable1,' variable and the ',x, ' assay.'))
+        function(x) {
+            if (!variable1 %in% names(se.obj@metadata[['metric']][[x]][[paste0('sil.', method)]])) {
+                stop(
+                    paste0(
+                        'The Silhouette based on ',
+                        method,
+                        ' has not been computed yet for the ',
+                        variable1,
+                        ' variable and the ',
+                        x,
+                        ' assay.'
+                    )
+                )
             }
-            se.obj@metadata[['metric']][[x]][[paste0('sil.',method)]][[variable1]]
+            se.obj@metadata[['metric']][[x]][[paste0('sil.', method)]][[variable1]]
         })
-    names(all.assays.var1)=levels(assay.names)
+    names(all.assays.var1) = levels(assay.names)
     all.assays.var1 <- as.data.frame(all.assays.var1)
-    all.assays.var1= all.assays.var1 %>% pivot_longer(
-        everything(),
-        names_to = 'datasets',
-        values_to = 'sil') %>% mutate(datasets = factor(
-            datasets,levels=levels(assay.names)))
+    all.assays.var1 = all.assays.var1 %>% pivot_longer(everything(),
+                                                       names_to = 'datasets',
+                                                       values_to = 'sil') %>% mutate(datasets = factor(datasets, levels =
+                                                                                                           levels(assay.names)))
     all.assays.var2 <- lapply(
         levels(assay.names),
-        function(x){
-            if (!variable2 %in% names(se.obj@metadata[['metric']][[x]][[paste0('sil.',method)]])) {
-                stop(paste0('The Silhouette based on ',method,' has not been computed yet for the ',variable2,' variable and the ',x, ' assay.'))
+        function(x) {
+            if (!variable2 %in% names(se.obj@metadata[['metric']][[x]][[paste0('sil.', method)]])) {
+                stop(
+                    paste0(
+                        'The Silhouette based on ',
+                        method,
+                        ' has not been computed yet for the ',
+                        variable2,
+                        ' variable and the ',
+                        x,
+                        ' assay.'
+                    )
+                )
             }
-            se.obj@metadata[['metric']][[x]][[paste0('sil.',method)]][[variable2]]
+            se.obj@metadata[['metric']][[x]][[paste0('sil.', method)]][[variable2]]
         })
-    names(all.assays.var2)=levels(assay.names)
+    names(all.assays.var2) = levels(assay.names)
     all.assays.var2 <- as.data.frame(all.assays.var2)
-    all.assays.var2= all.assays.var2 %>% pivot_longer(
-        everything(),
-        names_to = 'datasets',
-        values_to = 'sil') %>% mutate(datasets = factor(
-            datasets,levels=levels(assay.names)))
+    all.assays.var2 = all.assays.var2 %>% pivot_longer(everything(),
+                                                       names_to = 'datasets',
+                                                       values_to = 'sil') %>% mutate(datasets = factor(datasets, levels =
+                                                                                                           levels(assay.names)))
 
-    df=cbind(all.assays.var1,all.assays.var2)
-    everything<-datasets<-Silh1<-Silh2<-NULL
-    colnames(df)=c("datasets","Silh1","datasets","Silh2")
-    df=df[,-3]
+    df = cbind(all.assays.var1, all.assays.var2)
+    everything <- datasets <- Silh1 <- Silh2 <- NULL
+    colnames(df) = c("datasets", "Silh1", "datasets", "Silh2")
+    df = df[,-3]
 
     ## Plot
-    p=ggplot(df,aes(Silh1,Silh2,colour=datasets)) +
+    p = ggplot(df, aes(Silh1, Silh2, colour = datasets)) +
         geom_point(aes(color = datasets), size = 3) +
-        xlab(paste('Silhouette based on ',variable1,sep="")) +
-        ylab (paste('Silhouette based on ',variable2,sep="")) +
+        xlab(paste('Silhouette based on ', variable1, sep = "")) +
+        ylab (paste('Silhouette based on ', variable2, sep = "")) +
         theme(
-        panel.background = element_blank(),
-        axis.line = element_line(colour = 'black', size = 1),
-        axis.title.x = element_text(size = 18),
-        axis.title.y = element_text(size = 18),
-        axis.text.x = element_text(size = 12),
-        axis.text.y = element_text(size = 12),
-        legend.text = element_text(size = 12),
-        legend.title = element_text(size = 16))+
+            panel.background = element_blank(),
+            axis.line = element_line(colour = 'black', size = 1),
+            axis.title.x = element_text(size = 18),
+            axis.title.y = element_text(size = 18),
+            axis.text.x = element_text(size = 12),
+            axis.text.y = element_text(size = 12),
+            legend.text = element_text(size = 12),
+            legend.title = element_text(size = 16)
+        ) +
         ggtitle("Combined silhouette coefficients")
 
-    if  (length(levels(assay.names)) <= 4){
-        dataSets.colors <- wes_palette(
-        n = length(levels(assay.names)),
-        name = "GrandBudapest1")[seq(1:length(levels(assay.names)))]
-        p=p+scale_color_manual(
-            values=c(dataSets.colors),
-            name = 'Datasets')
+    if (length(levels(assay.names)) <= 4) {
+        dataSets.colors <- wes_palette(n = length(levels(assay.names)),
+                                       name = "GrandBudapest1")[seq(1:length(levels(assay.names)))]
+        p = p + scale_color_manual(values = c(dataSets.colors),
+                                   name = 'Datasets')
     }
 
     printColoredMessage(message = '------------The plotCombinedSilhouette function finished.',
-                    color = 'white',
-                    verbose = verbose)
-return(p)
+                        color = 'white',
+                        verbose = verbose)
+    return(p)
 
 }
