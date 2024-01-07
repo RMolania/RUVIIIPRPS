4aedd07301d6e36c81eeae738999e11b93d79198
RMolania <ramyar.molania@gmail.com>
2024-01-07 15:58:17 +1100

    remove extra space

--- R/plotPCAAssay.R
+++ R/plotPCAAssay.R
@@ -15,6 +15,7 @@
 #' @param alpha geom_point aesthetics
 #'
 #' @return plot PCA plot of the data colored by one variable
+
 #' @importFrom ggpubr get_legend as_ggplot
 #' @importFrom grid textGrob
 #' @importFrom cowplot axis_canvas ggdraw insert_xaxis_grob insert_yaxis_grob
