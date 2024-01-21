aea0bda4e7d6b94efc04f9c34c4bd285b7aaf5ff
RMolania <ramyar.molania@gmail.com>
2024-01-22 09:55:40 +1100

    add more explnation and details

--- R/computeRLE.R
+++ R/computeRLE.R
@@ -1,10 +1,10 @@
-#' is used to calculate and plot relative log expression (RLE) of RNA-seq data.
+#' is used to calculate relative log expression (RLE) of RNA-seq data.
 
 #' @author Ramyar Molania
 
 #' @description
 #' This functions calculates relative log expression (RLE) of the assay(s) in a SummarizedExperiment object. In addition,
-#' the function returens the medians,interquartiles and boxplots of indivdial assay's RLE.
+#' the function returns the medians and interquartiles of RLE boxplots for indivdial assay's RLE.
 
 #' @details
 #' RLE plots23 are used to reveal trends, temporal clustering and other non-random patterns resulting from unwanted variation
@@ -29,7 +29,6 @@
 #' note, any RNA-seq data (assays) must be in log scale before computing RLE.
 #' @param pseudo.count Numeric. A value as a pseudo count to be added to all measurements of the assay(s) before applying
 #' log transformation to avoid -Inf for measurements that are equal to 0. The default is 1.
-#' @param ylim.rle.plot Numeric. A vector of two values to specify the ylim of the RLE plot.
 #' @param outputs.to.returns Symbol. A symbol or list of symbols indicating what kind of outputs should be returened by
 #' the function. The options are "all", "rle.data", "rle.plot". If "all" is selected, the function returns the RLE data,
 #' medians and interquartiles of the RLE data and the RLE plot. If "rle.data", the function returns all the outputs except
@@ -39,12 +38,11 @@
 #' @param remove.na Symbol. To remove NA or missing values from the assays or not. The options are 'assays' and 'none'.
 #' The default is "assays", so all the NA or missing values from the assay(s) will be removed before computing RLE. See
 #' the checkSeObj function for more details.
-#' @param plot.output Logical. If TRUE, the RLE plots will be printed out.
 #' @param save.se.obj Logical. Indicates whether to save the result in the metadata of the SummarizedExperiment object or
 #' to output the result. By default it is set to TRUE.
 #' @param verbose Logical. If TRUE, displaying process messages is enabled.
 
-#' @return A SummarizedExperiment object or a list of the computed RLE and the associated plots.
+#' @return A SummarizedExperiment object or a list that contains all RLE data of individual assay(s).
 
 #' @importFrom SummarizedExperiment assays assay
 #' @importFrom matrixStats rowMedians colMedians colIQRs
@@ -54,48 +52,29 @@
 
 computeRLE <- function(
         se.obj,
-        assay.names = "All",
+        assay.names = "all",
         apply.log = TRUE,
         pseudo.count = 1,
-        ylim.rle.plot = c(-3,3),
         outputs.to.returns = 'all',
-        plot.output = FALSE,
         assess.se.obj = TRUE,
         remove.na = 'assays',
         save.se.obj = TRUE,
         verbose = TRUE) {
-    printColoredMessage(message = '------------The plotRLE function starts:',
+    printColoredMessage(message = '------------The computeRLE function starts:',
                         color = 'white',
                         verbose = verbose)
 
     # check inputs ####
-    if (length(assay.names) == 1 & assay.names != 'all') {
-        if (!assay.names %in% names(assays(se.obj)))
-            stop('The assay name cannot be found in the SummarizedExperiment object.')
-    }
-    if (length(assay.names) > 1) {
-        if (!assay.names %in% names(assays(se.obj)))
-            stop('The assay names cannot be found in the SummarizedExperiment object.')
-    }
     if (apply.log){
         if (pseudo.count < 0)
             stop('The value of "pseudo.count" cannot be negative.')
     }
-    if (is.null(ylim.rle.plot) | length(ylim.rle.plot) != 2){
-        stop('Please specify the "ylim.rle.plot" argument.')
-    }
-    if (sum(outputs.to.returns == 'none') > 0 & length(outputs.to.returns) > 1){
-        stop('The "outputs.to.returns" cannot contain "none" and some data outputs: "all", "rle.data" or "rle.plot".')
-    }
-    if (sum(outputs.to.returns %in% c('all', 'rle.data', 'rle.plot', 'none')) != length(outputs.to.returns)) {
-        stop('The "outputs.to.returns" should be in "all", "rle.data", "rle.plot" and "none".')
+    if (sum(outputs.to.returns %in% c('all', 'rle.data')) != length(outputs.to.returns)) {
+        stop('The "outputs.to.returns" must be in on of the "all" or "rle.data".')
     }
 
-    # pseudo count ####
-    if(is.null(pseudo.count)) pseudo.count == 0
-
-    # find assays ####
-    if (length(assay.names) == 1 && assay.names == 'All') {
+    # assays ####
+    if (length(assay.names) == 1 && assay.names == 'all') {
         assay.names <- as.factor(names(assays(se.obj)))
     } else  assay.names <- as.factor(unlist(assay.names))
 
@@ -106,33 +85,29 @@ computeRLE <- function(
             assay.names = assay.names,
             variables = NULL,
             remove.na = remove.na,
-            verbose = verbose)
-    }
-    # compute rle for each assays ####
+            verbose = verbose)}
+
+    # data transformation ####
     printColoredMessage(
-        message = paste0('-- Compute RLE:'),
+        message = paste0('-- Data transformation:'),
         color = 'magenta',
         verbose = verbose)
-    rle.all <- lapply(
+    all.assays <- lapply(
         levels(assay.names),
-        function(x) {
-            printColoredMessage(
-                message = paste0('Compute RLE on the ', x, ' assay.'),
-                color = 'blue',
-                verbose = verbose)
+        function(x){
             # log transformation ####
             if (apply.log & !is.null(pseudo.count)) {
                 printColoredMessage(
-                    message = paste0('Apply log2 + ', pseudo.count,  ' transformation on the', x, ' assay.'),
+                    message = paste0('Apply log2 + ', pseudo.count,  ' (pseudo.count) on the ', x, ' assay.'),
                     color = 'blue',
                     verbose = verbose)
                 expr <- log2(assay(x = se.obj, i = x) + pseudo.count)
             } else if (apply.log & is.null(pseudo.count)){
                 printColoredMessage(
-                    message = paste0('Apply log2 transformation on the', x, ' assay.'),
+                    message = paste0('Apply log2 on the ', x, ' assay.'),
                     color = 'blue',
                     verbose = verbose)
-                expr <- log2(assay(x = se.obj, i = x) + pseudo.count)
+                expr <- log2(assay(x = se.obj, i = x))
             } else {
                 printColoredMessage(
                     message = paste0(
@@ -146,8 +121,27 @@ computeRLE <- function(
                     color = 'red',
                     verbose = verbose)
                 expr <- assay(x = se.obj, i = x)
-            }
-            rle.data <- expr - rowMedians(expr)
+                }
+        })
+    names(all.assays) <- levels(assay.names)
+
+    # compute rle for each assays ####
+    printColoredMessage(
+        message = paste0('-- Compute the RLE:'),
+        color = 'magenta',
+        verbose = verbose)
+    all.assays <- lapply(
+        levels(assay.names),
+        function(x) {
+            printColoredMessage(
+                message = paste0('Compute the RLE on the ', x, ' assay.'),
+                color = 'blue',
+                verbose = verbose)
+            rle.data <- all.assays[[x]] - rowMedians(all.assays[[x]])
+            printColoredMessage(
+                message = '- Obtain the RLE medians and interquartile range on from the RLE data.',
+                color = 'blue',
+                verbose = verbose)
             rle.med <- colMedians(rle.data)
             rle.iqr <- colIQRs(rle.data)
             rle <- list(
@@ -155,66 +149,34 @@ computeRLE <- function(
                 rle.med = rle.med,
                 rle.iqr = rle.iqr)
         })
-    names(rle.all) <- levels(assay.names)
-
-    # plot rle ####
-    samples <- rle <- everything <- NULL
-    plot.rle <- lapply(
-        levels(assay.names),
-        function(x) {
-            tmp <- rle.all[[x]]$rle.data
-            rle.data <- data.frame(
-                samples = rep(seq_len(ncol(tmp)), each = nrow(tmp)),
-                rle = as.numeric(tmp))
-            p.rle <- ggplot(rle.data, aes(x = samples, y = rle, group = samples)) +
-                geom_boxplot(outlier.size = -1) +
-                ylab('RLE') +
-                xlab('Samples') +
-                geom_hline(yintercept = 0) +
-                coord_cartesian(ylim = ylim.rle.plot) +
-                stat_summary(
-                    color = "red",
-                    size = .1,
-                    fun.data = function(x) {
-                        c(y = median(x),
-                          ymin = median(x),
-                          ymax = median(x))}) +
-                theme(panel.background = element_blank(),
-                      axis.line = element_line(colour = 'black'),
-                      axis.title.x = element_text(size = 14),
-                      axis.title.y = element_text(size = 14),
-                      axis.text.x = element_text(size = 10),
-                      axis.text.y = element_text(size = 10)) +
-                ggtitle(paste0('RLE plots of the', x, ' data.'))
-            p.rle
-        })
-    names(plot.rle) <- levels(assay.names)
+    names(all.assays) <- levels(assay.names)
 
     # outputs to save ####
     if(outputs.to.returns == 'all'){
-        outputs <- lapply(
+        all.assays <- lapply(
             levels(assay.names),
             function(x){
                 list(
-                    rle.data = rle.all[[x]]$rle.data,
-                    rle.medians = rle.all[[x]]$rle.med,
-                    rle.iqrs = rle.all[[x]]$rle.iqr,
-                    rle.plot = plot.rle[[x]]
+                    rle.data = all.assays[[x]]$rle.data,
+                    rle.medians = all.assays[[x]]$rle.med,
+                    rle.iqrs = all.assays[[x]]$rle.iqr
                     )
             })
-        names(outputs) <- levels(assay.names)
+        names(all.assays) <- levels(assay.names)
     } else if (outputs.to.returns == 'rle.data'){
-        outputs <- rle.all
-    } else if (outputs.to.returns == 'rle.plot'){
-        outputs <- plot.rle
+        all.assays <- lapply(
+            levels(assay.names),
+            function(x) rle.data = all.assays[[x]]$rle.data
+            )
+        names(all.assays) <- levels(assay.names)
     }
     # save the results ####
     ## add results to the SummarizedExperiment object ####
     if (save.se.obj == TRUE) {
-        printColoredMessage(message = '-- Save the RLE plot to the metadata of the SummarizedExperiment object.',
+        printColoredMessage(message = '-- Save the RLE data to the metadata of the SummarizedExperiment object.',
                             color = 'magenta',
                             verbose = verbose)
-        ## For all assays
+        ## for all assays
         for (x in levels(assay.names)) {
             ## check if metadata metric already exist
             if (length(se.obj@metadata) == 0) {
@@ -232,22 +194,26 @@ computeRLE <- function(
             if (!'RLE' %in% names(se.obj@metadata[['metric']][[x]])) {
                 se.obj@metadata[['metric']][[x]][['RLE']] <- list()
             }
-            se.obj@metadata[['metric']][[x]][['RLE']] <- outputs[[x]]
+            ## check if metadata metric already exist for this assay and this metric
+            if (!'rle.data' %in% names(se.obj@metadata[['metric']][[x]][['RLE']])) {
+                se.obj@metadata[['metric']][[x]][['RLE']][['rle.data']] <- list()
+            }
+            se.obj@metadata[['metric']][[x]][['RLE']][['rle.data']] <- all.assays[[x]]
         }
         printColoredMessage(
-            message = paste0('The RLE plots are saved to metadata@$plot$rle'),
+            message = paste0('The RLE data of individual assay is saved to metadata@metric'),
             color = 'blue',
             verbose = verbose
         )
-        printColoredMessage(message = '------------The plotRLE function finished.',
+        printColoredMessage(message = '------------The computeRLE function finished.',
                             color = 'white',
                             verbose = verbose)
         return(se.obj = se.obj)
     } else if (save.se.obj == FALSE) {
-        printColoredMessage(message = '------------The plotRLE function finished.',
+        printColoredMessage(message = '------------The computeRLE function finished.',
                             color = 'white',
                             verbose = verbose)
-        return(rle = outputs)
+        return(rle.data = all.assays)
     }
 
 }
